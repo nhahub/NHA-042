90990c1c089c7969c2b6a142ad1a0295bf05db06
MarkSafwatWilliam <marksafwat78@gmail.com>
2025-11-22 14:16:40 +0200

    Added Models (Doctor, Clinic,Specialization)

---
+++ Online_Medical/Models/Clinic.cs
@@ -0,0 +1,12 @@
+﻿namespace Online_Medical.Models
+{
+    public class Clinic
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ Online_Medical/Models/Doctor.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Online_Medical.Models
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+    }
+    public class Doctor
+    {
+
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+
+        public Gender Gender { get; set; }
+        public int ExperienceYears { get; set; }
+
+        public string Bio { get; set; }
+
+        public string ProfileImage { get; set; }
+
+        [ForeignKey("Specialization")]
+        public int SpecializationId { get; set; }
+        public Specialization Specialization { get; set; }
+
+        //public double Fees {get; set;}
+
+        //Relation:
+    }
+}
